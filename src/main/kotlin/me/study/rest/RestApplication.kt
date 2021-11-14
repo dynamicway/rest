31fb397c7b337d5e62575a1e0b694baf23bb27ce
bee <rkddbtjd123@gmail.com>
2021-11-14 17:53:52 +0900

    init spring rest api

---
+++ src/main/kotlin/me/study/rest/RestApplication.kt
@@ -0,0 +1,11 @@
+package me.study.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApplication>(*args)
+}
